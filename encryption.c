6d965e44d742bcfb048ca5460d9029284e6277ad
Shuvo Paul <shuvopaul@yahoo.com>
2022-06-26 18:21:39 +0000

    encryption

---
+++ encryption.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void encrypt(char msg[]) {
+    int i, n;
+
+    n = strlen(msg);
+
+    for(i = 0; i < n; i++) {
+        if((msg[i] >= 'a' && msg[i] <= 'z') || (msg[i] >= 'A' && msg[i] <= 'Z')) {
+            if(msg[i] == 'z' || msg[i] == 'Z') {
+                msg[i] = msg[i] - 25;
+                continue;
+            }
+            msg[i] = msg[i] + 1;
+        }
+    }
+}
+
+int main() {
+    char s[80];
+
+    scanf("%s", s);
+    encrypt(s);
+    printf("%s\n", s);
+    return 0;
+}
