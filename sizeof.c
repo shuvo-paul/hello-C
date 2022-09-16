1261eb8bb7d822f1911cbb7f7d238a7fe4d8edec
Shuvo Paul <shuvopaul@yahoo.com>
2022-09-17 00:19:01 +0600

    sizeof test

---
+++ sizeof.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    char ch;
+    double d_num;
+    float f_num;
+
+    printf("%d\n", sizeof(int));
+    printf("size of int: %d\n", sizeof(num));
+    printf("size of char: %d\n", sizeof(ch));
+    printf("size of d_num: %d\n", sizeof(num));
+    printf("size of f_num: %d\n", sizeof(num));
+
+    return 0;
+}
